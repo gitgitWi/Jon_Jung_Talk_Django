1ec03c43e93b96e06c30da2d658dc437b5c01bcf
ezerwi <57997672+ezerwi@users.noreply.github.com>
2020-06-14 16:23:20 +0000

    add whitenoise to ignore static in deploying

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,11 @@
 import os
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
