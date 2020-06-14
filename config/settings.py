fd8ff950ac6856516441061290c813f1ae75249c
ezerwi <57997672+ezerwi@users.noreply.github.com>
2020-06-14 17:15:21 +0000

    commented whitenoise in settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,9 @@
 import os
 
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-)
+# MIDDLEWARE_CLASSES = (
+#     # Simplified static file serving.
+#     'whitenoise.middleware.WhiteNoiseMiddleware'
+# )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -120,4 +120,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
